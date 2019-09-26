20114c335f51744d54ef635d47aa68835741a71b
Gerardo Pacheco Sicart <gerardo.sicart@gmail.com>
2019-09-26 08:48:21 +0200

    Mobile - Platform mock

--- packages/edit-post/src/test/editor.native.js
+++ packages/edit-post/src/test/editor.native.js
@@ -11,6 +11,7 @@ import { registerCoreBlocks } from '@wordpress/block-library';
 import { store } from '@wordpress/editor'; // eslint-disable-line no-unused-vars
 
 jest.mock( '../components/layout', () => () => 'Layout' );
+jest.mock( 'Platform', () => ( { select: () => '' } ) );
 
 /**
  * Internal dependencies
